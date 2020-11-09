24dd755d4769d25ee3c311b218f5612cecf42bd1
Paulo Alexandre <paulofore@gmail.com>
2020-11-09 13:16:06 -0300

    sisacao

--- sisacao-server/common/models/ativo-acao.js
+++ sisacao-server/common/models/ativo-acao.js
@@ -3,15 +3,33 @@
 module.exports = function (Ativoacao) {
 
 
-/**
- * 
- * @param {Function(Error, array)} callback
- */
+    /**
+     * 
+     * @param {Function(Error, array)} callback
+     */
 
     Ativoacao.ListaColetaIntraday = function (callback) {
         var lista;
         // TODO
         callback(null, lista);
     };
+
+
+    /**
+    * 
+    * @param {string} ticker 
+    * @param {string} nome 
+    * @param {string} tipo 
+    * @param {number} quantidadeTeorica 
+    * @param {number} participacao 
+    * @param {string} indice 
+    * @param {Function(Error, object)} callback
+    */
+    Ativoacao.InsereAcaoIndice = function (ticker, nome, tipo, quantidadeTeorica, participacao, indice, callback) {
+        var saida;
+        // TODO
+        callback(null, saida);
+    };
+
 
 };
